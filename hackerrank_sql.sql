269c487a62899231ed213fc121ce4a5ebce2994d
adam-talbot <adam.talbot527@gmail.com>
2022-10-21 16:18:23 -0600

    finished 2 sql problems

--- hackerrank_sql.sql
+++ hackerrank_sql.sql
@@ -7,3 +7,15 @@ group by earnings
 order by earnings desc
 limit 1;
 
+/* Query the following two values from the STATION table:
+The sum of all values in LAT_N rounded to a scale of  decimal places.
+The sum of all values in LONG_W rounded to a scale of  decimal places. */
+
+select round(sum(lat_n), 2), round(sum(long_w), 2)
+from STATION;
+
+-- Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than  and less than . Truncate your answer to  decimal places.
+
+select truncate(sum(lat_n), 4)
+from STATION
+where lat_n between 38.7880 and 137.2345;
