d492a965c640869de026c3638f45e4a0b576766c
adam-talbot <adam.talbot527@gmail.com>
2022-10-24 17:05:54 -0600

    another easy sql problem completed

--- hackerrank_sql.sql
+++ hackerrank_sql.sql
@@ -30,3 +30,10 @@ from employees;
 select truncate(max(lat_n), 4)
 from station
 where lat_n < 137.2345;
+
+-- Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than . Round your answer to  decimal places.
+select round(long_w, 4)
+from station
+where lat_n < 137.2345
+order by lat_n desc
+limit 1;
