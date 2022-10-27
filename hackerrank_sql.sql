f8fbc480aa6c433b9df7007b08f0687a751cda80
adam-talbot <adam.talbot527@gmail.com>
2022-10-27 16:47:22 -0600

    finished a few more sql problems

--- hackerrank_sql.sql
+++ hackerrank_sql.sql
@@ -74,3 +74,26 @@ from
 group by rn;
 
 -- did some more station problems similar to those above
+
+-- Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
+-- Note: CITY.CountryCode and COUNTRY.Code are matching key
+
+select sum(city.population)
+from city
+join country on city.countrycode = country.code
+where continent = 'Asia';
+
+-- Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
+-- Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+
+select city.name
+from city
+join country on city.countrycode = country.code
+where continent = 'Africa';
+
+-- Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
+
+select continent, floor(avg(city.population))
+from city
+join country on city.countrycode = country.code
+group by continent;
