975f2315b9812c48af7349855065ecc827db9439
adam-talbot <adam.talbot527@gmail.com>
2022-11-08 22:27:08 -0700

    completed contest leaderboard sql question

--- hackerrank_sql.sql
+++ hackerrank_sql.sql
@@ -193,3 +193,19 @@ having count > 1
 order by count desc, s.hacker_id) as t1;
 
 -- could also potentially use case statement to get full marks column and filter on that instead of using and condition in join statement but gets more complicated since select is performed after where
+
+-- Contest Leaderboard
+-- You did such a great job helping Julia with her last coding contest challenge that she wants you to work on this one, too!
+-- The total score of a hacker is the sum of their maximum scores for all of the challenges. Write a query to print the hacker_id, name, and total score of the hackers 
+-- ordered by the descending score. If more than one hacker achieved the same total score, then sort the result by ascending hacker_id. 
+-- Exclude all hackers with a total score of  from your result.
+
+select hacker_id, name, sum(max_score) as sum
+from
+(select name, hacker_id, challenge_id, max(score) as max_score
+from submissions as s
+join hackers as h using(hacker_id)
+group by name, hacker_id, challenge_id) as t1
+group by hacker_id, name
+having sum > 0
+order by sum desc, hacker_id;
