b92e553dd2a47adb9e3875e64d3b6a1c83ebb73c
adam-talbot <adam.talbot527@gmail.com>
2022-11-11 22:32:24 -0700

    finished placements sql problem

--- hackerrank_sql.sql
+++ hackerrank_sql.sql
@@ -209,3 +209,17 @@ group by name, hacker_id, challenge_id) as t1
 group by hacker_id, name
 having sum > 0
 order by sum desc, hacker_id;
+
+-- Placements
+-- You are given three tables: Students, Friends and Packages. Students contains two columns: ID and Name. Friends contains two columns: ID and Friend_ID (ID of the ONLY best friend). 
+-- Packages contains two columns: ID and Salary (offered salary in $ thousands per month).
+-- Write a query to output the names of those students whose best friends got offered a higher salary than them. Names must be ordered by the salary amount offered to the best friends. 
+-- It is guaranteed that no two students got same salary offer.
+
+select s.name
+from students as s
+join friends as f using(id)
+join packages as p using(id)
+join packages as p2 on p2.id = f.friend_id
+where p.salary < p2.salary
+order by p2.salary;
